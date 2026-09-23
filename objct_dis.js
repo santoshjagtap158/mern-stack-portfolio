56c43083bd92b398d66144a8c01e7020ef9a922d
Santosh Jagtap <santoshjagatap30@gmail.com>
2026-09-23 21:46:51 +0530

    day 1 and 2 of week 1

--- objct_dis.js
+++ objct_dis.js
@@ -3,4 +3,5 @@ const car = { brand: "Tesla", model: "Model 3", color: "white" };
 const { brand, model } = car;
 
 console.log("Brand:", brand);   // Tesla
+
 console.log("Model:", model);   // Model 3
